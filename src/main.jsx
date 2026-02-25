96eb9e03ef06d6e8bd1bc2cfaf3a0669ea3dc91f
babithbhoop <48808822+babithbhoop@users.noreply.github.com>
2026-02-25 08:31:24 -0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import SparkleSpaceApp from './App.jsx'
+
+createRoot(document.getElementById('root')).render(<SparkleSpaceApp />)
